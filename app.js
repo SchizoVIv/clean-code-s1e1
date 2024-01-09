c8f01a1d5fe23c01097f306130a751b11005d5b0
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-01-09 21:23:47 +0200

    feat: add alert

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
 
+alert("Я по привычке добавли ссылку на ghPages и забыла заменить на нужную. Вот ссылка на запрос https://github.com/SchizoVIv/clean-code-s1e1/pull/3 , или можно перейти сразу на мой гитхаб и увидеть нужный епозиторий, извини за неудобство")
+
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
 //Break things down into smaller steps and take each step at a time.
